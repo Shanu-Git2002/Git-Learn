42444632ab36b16fbbeb10677cfedf2e179d1d5c
shanu kumari <shanukum2002@gmail.com>
2025-04-19 22:12:00 +0530

    D-3

--- text-2.jsx
+++ text-2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Text2 = () => {
   return (
-    <div>text-2 main shanu merge with development for S-2</div>
+    <div>text-2 main shanu merge with development for D-3</div>
   )
 }
 
